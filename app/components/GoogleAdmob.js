4bd9abb1d4aaed27754efc8bbfc028d0df3b5014
HoWWWWWhy <HoWWWWWhy@gmail.com>
2022-01-31 00:47:03 +0900

    react-native-firebase/admob bugfix and patch file added

--- app/components/GoogleAdmob.js
+++ app/components/GoogleAdmob.js
@@ -16,42 +16,55 @@ const adUnitId = __DEV__
 const BannerAdMaxHeight = 100;
 
 const MyBannerAd = () => {
-  const [showBanner, setShowBanner] = useState(false);
-
+  const [showBanner, setShowBanner] = useState(true);
+  const [altanativeText, setAltanativeText] = useState('');
   useEffect(() => {
-    admob()
-      .setRequestConfiguration({
-        // Update all future requests suitable for parental guidance
-        maxAdContentRating: MaxAdContentRating.G,
-
-        // Indicates that you want your content treated as child-directed for purposes of COPPA.
-        tagForChildDirectedTreatment: false,
-
-        // Indicates that you want the ad request to be handled in a
-        // manner suitable for users under the age of consent.
-        tagForUnderAgeOfConsent: true,
-      })
-      .then(() => {
-        // Request config successfully set!
-        setShowBanner(true);
-      });
+    //console.log('MyBannerAd Mounted:', adUnitId);
+    setAdRequestConfig();
   }, []);
+
+  const setAdRequestConfig = async () => {
+    //console.log('CALL setConfig');
+    await admob().setRequestConfiguration({
+      // Update all future requests suitable for parental guidance
+      maxAdContentRating: MaxAdContentRating.G,
+
+      // Indicates that you want your content treated as child-directed for purposes of COPPA.
+      tagForChildDirectedTreatment: false,
+
+      // Indicates that you want the ad request to be handled in a
+      // manner suitable for users under the age of consent.
+      tagForUnderAgeOfConsent: true,
+    });
+    // Request config successfully set!
+    //console.log('Show Banner!');
+    //setShowBanner(true);
+    //setAltanativeText('광고 요청 중');
+  };
+
   return (
-    <View style={styles.container}>
-      {/* <Text>광고 자리입니다.</Text> */}
+    <View
+      style={
+        showBanner ? styles.container : [styles.container, styles.blankAd]
+      }>
       {showBanner ? (
         <BannerAd
           unitId={adUnitId}
-          size={BannerAdSize.LARGE_BANNER}
+          size={BannerAdSize.LARGE_BANNER} //Large banner: 320 x 100 density-independent pixels
           requestOptions={{
             requestNonPersonalizedAdsOnly: false,
           }}
           onAdFailedToLoad={error => {
             console.log('Advert failed to load: ', error);
+            setShowBanner(false);
+            setAltanativeText('광고 자리입니다.');
           }}
+          onAdLoaded={() => console.log('onAdLoaded')}
         />
       ) : (
-        <Text>광고 자리입니다.</Text>
+        <View style={styles.textContainer}>
+          <Text style={styles.textBlankAd}>{altanativeText}</Text>
+        </View>
       )}
     </View>
   );
@@ -60,12 +73,23 @@ const MyBannerAd = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    //borderStyle: 'solid',
-    //borderWidth: 1,
-    //borderColor: 'black',
     justifyContent: 'flex-end',
     alignItems: 'center',
-    //backgroundColor: 'white',
+  },
+  blankAd: {
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: 'black',
+    backgroundColor: 'white',
+    //paddingBottom: 45,
+  },
+  textContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textBlankAd: {
+    fontSize: 20,
   },
 });
 
